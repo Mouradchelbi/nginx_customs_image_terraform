5961105841c697caa6b566444c2e3a2e7f1a3bb0
Mouradchelbi <72913289+Mouradchelbi@users.noreply.github.com>
2023-06-09 12:15:45 +0200

    Update variables.tf

--- ProdEnvironment/variables.tf
+++ ProdEnvironment/variables.tf
@@ -3,16 +3,6 @@ variable "subscription_id" {
    default = "393e3de3-0900-4b72-8f1b-fb3b1d6b97f1"
 }
 
-variable "client_id" {
-      description = "Azure Client ID"
-default = "99208659-9294-464c-afa1-70db52148203"
-}
-
-variable "client_secret" {
-description = "Azure Client Secret"
-default = "3bh8Q~U05J0MOm3k.MOdhJEpf7TVp6iqApQpIbpS"
-}
-
 variable "tenant_id" {
    description = "Azure Tenant ID"
    default = "7349d3b2-951f-41be-877e-d8ccd9f3e73c"
