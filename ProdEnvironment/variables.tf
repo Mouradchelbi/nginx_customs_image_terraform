b778948adc80e14b414ea3be22f48e7abada3922
Mouradchelbi <72913289+Mouradchelbi@users.noreply.github.com>
2023-06-09 11:39:28 +0200

    Update variables.tf

--- ProdEnvironment/variables.tf
+++ ProdEnvironment/variables.tf
@@ -3,6 +3,15 @@ variable "subscription_id" {
    default = "393e3de3-0900-4b72-8f1b-fb3b1d6b97f1"
 }
 
+variable "client_id" {
+      description = "Azure Client ID"
+default = "99208659-9294-464c-afa1-70db52148203"
+}
+
+variable "client_secret" {
+description = "Azure Client Secret"
+default = "3bh8Q~U05J0MOm3k.MOdhJEpf7TVp6iqApQpIbpS"
+}
 
 variable "tenant_id" {
    description = "Azure Tenant ID"
