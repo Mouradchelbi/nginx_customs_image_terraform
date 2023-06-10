ca0cd60cef09eda1a0f6c18777d2abfb6e740f3e
Mouradchelbi <72913289+Mouradchelbi@users.noreply.github.com>
2023-06-10 11:43:24 +0200

    Update variables.tf

--- azure-webserver/variables.tf
+++ azure-webserver/variables.tf
@@ -1,21 +1,18 @@
 variable "subscription_id" {
-   description = "Azure subscription"
-   default = "393e3de3-0900-4b72-8f1b-fb3b1d6b97f1"
+  type = string
 }
 
 variable "client_id" {
-      description = "Azure Client ID"
-default = "99208659-9294-464c-afa1-70db52148203"
+  type = string
 }
 
 variable "client_secret" {
-description = "Azure Client Secret"
-default = "3bh8Q~U05J0MOm3k.MOdhJEpf7TVp6iqApQpIbpS"
+  type = string
 }
 
 variable "tenant_id" {
-   description = "Azure Tenant ID"
-   default = "7349d3b2-951f-41be-877e-d8ccd9f3e73c"
+  type = string
+}
 }
 
 variable "instance_size" {
