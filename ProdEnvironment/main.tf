c1cafb6a43778bb50f9d159f6cc36551c07cff80
Mouradchelbi <72913289+Mouradchelbi@users.noreply.github.com>
2023-06-10 13:52:26 +0200

    Update main.tf

--- ProdEnvironment/main.tf
+++ ProdEnvironment/main.tf
@@ -2,10 +2,6 @@ module  "Prod" {
   source        = "../azure-webserver"
   environment   = "Prod"
   Resource_Group_name = "CMProdRGn"
-  subscription_id = var.subscription_id
-   client_id = var.client_id
-   client_secret = var.client_secret
-   tenant_id = var.tenant_id
-   }
+     }
   
   
