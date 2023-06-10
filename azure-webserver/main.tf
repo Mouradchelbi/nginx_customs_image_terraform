0d3bbf900876de1da841a429103f8eea27176261
Mouradchelbi <72913289+Mouradchelbi@users.noreply.github.com>
2023-06-10 12:43:22 +0200

    Update main.tf

--- azure-webserver/main.tf
+++ azure-webserver/main.tf
@@ -3,6 +3,14 @@ resource "azurerm_resource_group" "webserver" {
   location = "${var.location}"
 }
 
+provider "azurerm" {
+   subscription_id = var.subscription_id
+   client_id = var.client_id
+   client_secret = var.client_secret
+   tenant_id = var.tenant_id
+   skip_provider_registration = true
+   features {}
+}
 resource "azurerm_network_security_group" "allowedports" {
    name = "allowedports"
    resource_group_name = azurerm_resource_group.webserver.name
