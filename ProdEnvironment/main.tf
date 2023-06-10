380a717634809eb532a3aef6fcf05c2ac4ce00c4
Mouradchelbi <72913289+Mouradchelbi@users.noreply.github.com>
2023-06-10 13:03:06 +0200

    Update main.tf

--- ProdEnvironment/main.tf
+++ ProdEnvironment/main.tf
@@ -2,6 +2,13 @@ module  "Prod" {
   source        = "../azure-webserver"
   environment   = "Prod"
   Resource_Group_name = "CMProdRGn"
+  subscription_id = var.subscription_id
+   client_id = var.client_id
+   client_secret = var.client_secret
+   tenant_id = var.tenant_id
+   skip_provider_registration = true
+   features {}
+}
 }
   
   
